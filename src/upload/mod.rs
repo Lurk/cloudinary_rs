43988ceb6bfe583df2717da1bbdc28cddf4b01ea
Serhiy Barhamon <serhiy.barhamon@check24.de>
2025-11-16 22:04:51 +0100

    wip

--- src/upload/mod.rs
+++ src/upload/mod.rs
@@ -83,8 +83,15 @@ impl Upload {
             .send()
             .await
             .context(format!("upload to {}", url))?;
+        let status = response.status();
+        if status != 200 {
+            let text = response.text().await?;
+            return Err(anyhow::anyhow!("({}) <- {}\n\n{}", status, url, text));
+        }
         let text = response.text().await?;
-        let json = serde_json::from_str(&text).context(format!("failed to parse:\n\n {}", text))?;
+        let json = serde_json::from_str(&text)
+            .context(format!("({}) <- {}", status, url))
+            .context(format!("failed to parse:\n\n {}", text))?;
         Ok(json)
     }
 
